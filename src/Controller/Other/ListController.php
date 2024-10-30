0b344ab8ecbe261a278833158f72100d50844004
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-10-30 16:32:55 +0100

    Playlist

--- src/Controller/Other/ListController.php
+++ src/Controller/Other/ListController.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Repository\PlaylistRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class ListController extends AbstractController
 {
     #[Route(path: '/lists', name: 'my_lists')]
-    public function show(): Response
+    public function show(PlaylistRepository $playlistRepository): Response
     {
-        return $this->render('other/lists.html.twig');
+        $playlists = $playlistRepository->findAll();
+
+        return $this->render(
+            'other/lists.html.twig',
+            [
+                'playlists' => $playlists,
+            ]
+        );
     }
 }
