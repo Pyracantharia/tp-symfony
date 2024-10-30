733212b1fa0bb46409e54feeb513032b76e3a4ea
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-10-30 12:10:14 +0100

    adaptation de la movie card

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Repository\MovieRepository;
+
 class HomeController extends AbstractController
 {
     #[Route(path: '/', name: 'index')]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-        return $this->render('index.html.twig');
+        // Récupère les films à afficher
+        $movies = $movieRepository->findAll();
+
+        // Envoie les films au template index.html.twig
+        return $this->render('index.html.twig', [
+            'movies' => $movies
+        ]);
     }
 }
