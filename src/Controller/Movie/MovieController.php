d4b6ad5869f01aeaf26a2aed5829169aaaadc010
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-10-30 12:25:15 +0100

    On récupère et on passe les donnée de movie dans le front

--- src/Controller/Movie/MovieController.php
+++ src/Controller/Movie/MovieController.php
@@ -8,14 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Repository\MovieRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'show_movie')]
-    public function movie(): Response
+    #[Route('/movie/{id}', name: 'show_movie', requirements: ['id' => '\d+'])]
+    public function movie(int $id, MovieRepository $movieRepository): Response
     {
-        return $this->render('movie/detail.html.twig');
-    }
+        // Récupère le film par son id
+        $movie = $movieRepository->find($id);
 
+        // Gère le cas où le film n'existe pas
+        if (!$movie) {
+            throw $this->createNotFoundException('Le film n\'existe pas');
+        }
+
+        // Passe les informations du film au template
+        return $this->render('movie/detail.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+   
     #[Route('/serie', name: 'show_serie')]
     public function series(): Response
     {
