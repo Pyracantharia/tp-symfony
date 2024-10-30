1a391f2ebe7620db62fd25eec4c15dc0e53b02db
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-10-30 09:24:39 +0100

    voir les sub

--- src/Controller/Other/SubscriptionController.php
+++ src/Controller/Other/SubscriptionController.php
@@ -1,9 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller\Other;
 
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class SubscriptionController extends AbstractController
 {
     #[Route(path: '/subscriptions', name: 'subscriptions')]
-    public function show(): Response
+    public function show(SubscriptionRepository $subscriptionRepository): Response
     {
-        return $this->render('other/abonnements.html.twig');
+        $subscriptions = $subscriptionRepository->findAll();
+
+        return $this->render('other/abonnements.html.twig', [
+            'subscriptions' => $subscriptions,
+        ]);
     }
 }
